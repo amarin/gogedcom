21ea6656dd0fa63a5a5bac9aaae450fb0808ee2f
Marin Aleksey <asmadews@gmail.com>
2021-01-11 15:04:20 +0300

    added MaxRecordLevel constant and validate Level against it

--- pkg/record/level.go
+++ pkg/record/level.go
@@ -6,11 +6,15 @@ import (
 	"github.com/amarin/gogedcom/pkg/errors"
 )
 
+// MaxRecordLevel defines maximum allowed record level set to 99 according to GEDCOM 5.5.5 spec.
+const MaxRecordLevel = 99
+
+// Level type defines GEDCOM record level.
 type Level uint
 
 // Validate checks internal data is valid, returns error otherwise.
 func (level Level) Validate() error {
-	if level > 99 {
+	if level > MaxRecordLevel {
 		return fmt.Errorf("%w: level: must %v <= 99", errors.ErrValidation, level)
 	}
 
